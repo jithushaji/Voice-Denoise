f31a4a110745acc2ba5ec9bbb5adc4f7afccfc92
jithu <jithushaji23@gmail.com>
2021-06-16 10:01:49 +0530

    updated

--- predict.py
+++ predict.py
@@ -1,5 +1,5 @@
 import warnings
-
+import os
 import librosa
 import soundfile as sf
 import tensorflow as tf
@@ -34,34 +34,42 @@ loaded_model = model_from_json(loaded_model_json)
 loaded_model.load_weights('model.h5')
 print("Loaded model from disk")
 
-# Extracting noise and voice from folder and convert to numpy
-audio = audio_files_to_numpy(args['audio_dir_prediction'], args['audio_input_prediction'], args['sample_rate'],
-                             args['frame_length'], args['hop_length_frame'], args['min_duration'])
+dir_list = os.listdir(args['audio_dir_prediction'])
+ 
 
-#Dimensions of squared spectrogram
-dim_square_spec = int(args['n_fft'] / 2) + 1
-print(dim_square_spec)
+for noise in dir_list:
+# prints all files
+    print(noise)
+    voice =noise.split(" ") 
+    print(voice)
+    # Extracting noise and voice from folder and convert to numpy
+    audio = audio_files_to_numpy(args['audio_dir_prediction'], voice, args['sample_rate'],
+                                 args['frame_length'], args['hop_length_frame'], args['min_duration'])
 
-# Create Amplitude and phase of the sounds
-m_amp_db_audio,  m_pha_audio = numpy_audio_to_matrix_spectrogram(
-    audio, dim_square_spec, args['n_fft'], args['hop_length_fft'])
+    #Dimensions of squared spectrogram
+    dim_square_spec = int(args['n_fft'] / 2) + 1
+    print(dim_square_spec)
 
-#global scaling to have distribution -1/1
-X_in = scaled_in(m_amp_db_audio)
-#Reshape for prediction
-X_in = X_in.reshape(X_in.shape[0],X_in.shape[1],X_in.shape[2],1)
-#Prediction using loaded network
-X_pred = loaded_model.predict(X_in)
-#Rescale back the noise model
-inv_sca_X_pred = inv_scaled_ou(X_pred)
-#Remove noise model from noisy speech
-X_denoise = m_amp_db_audio - inv_sca_X_pred[:,:,:,0]
-#Reconstruct audio from denoised spectrogram and phase
-print(X_denoise.shape)
-print(m_pha_audio.shape)
-audio_denoise_recons = matrix_spectrogram_to_numpy_audio(X_denoise, m_pha_audio, args['frame_length'], args['hop_length_fft'])
-#Number of frames
-nb_samples = audio_denoise_recons.shape[0]
-#Save all frames in one file
-denoise_long = audio_denoise_recons.reshape(1, nb_samples * (args['frame_length']))*10
-sf.write(args['dir_save_prediction'] + args['audio_output_prediction'], denoise_long[0, :], args['sample_rate'])
+    # Create Amplitude and phase of the sounds
+    m_amp_db_audio,  m_pha_audio = numpy_audio_to_matrix_spectrogram(
+        audio, dim_square_spec, args['n_fft'], args['hop_length_fft'])
+
+    #global scaling to have distribution -1/1
+    X_in = scaled_in(m_amp_db_audio)
+    #Reshape for prediction
+    X_in = X_in.reshape(X_in.shape[0],X_in.shape[1],X_in.shape[2],1)
+    #Prediction using loaded network
+    X_pred = loaded_model.predict(X_in)
+    #Rescale back the noise model
+    inv_sca_X_pred = inv_scaled_ou(X_pred)
+    #Remove noise model from noisy speech
+    X_denoise = m_amp_db_audio - inv_sca_X_pred[:,:,:,0]
+    #Reconstruct audio from denoised spectrogram and phase
+    print(X_denoise.shape)
+    print(m_pha_audio.shape)
+    audio_denoise_recons = matrix_spectrogram_to_numpy_audio(X_denoise, m_pha_audio, args['frame_length'], args['hop_length_fft'])
+    #Number of frames
+    nb_samples = audio_denoise_recons.shape[0]
+    #Save all frames in one file
+    denoise_long = audio_denoise_recons.reshape(1, nb_samples * (args['frame_length']))*10
+    sf.write(args['dir_save_prediction'] + noise, denoise_long[0, :], args['sample_rate'])
